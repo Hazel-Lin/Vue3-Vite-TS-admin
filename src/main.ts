df2cec4e0a9f728ca29c418361b29713c9959073
linhuizi <529246402@qq.com>
2021-12-04 21:00:08 +0800

    build(mian): 调整左侧菜单栏

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+// import 'element-plus/dist/index.css'
+import 'element-plus/lib/theme-chalk/index.css';
 
 
 const app = createApp(App)
 
-app.use(ElementPlus)
 app.use(router)
+app.use(ElementPlus)
 app.mount('#app')
--- src/view/layout/component/Aside.vue
+++ src/view/layout/component/Aside.vue
@@ -1,33 +1,37 @@
 <template>
   <div class="mainSide">
-    <!-- <div class="logo">
-      <img class="img" src="https://cn.vitejs.dev/logo.svg" alt="logo" />
-      <span v-show="!isCollapse" class="title">Vue3Admin</span>
-    </div> -->
+    <div class="logo">
+      <img class="img" src="@/assets/logo.png" alt="logo" />
+      <span class="title">Vue3Admin</span>
+    </div>
     <el-menu
+      :collapse="collapse"
+      background-color="#0c2135"
+      text-color="#b7bdc3"
+      active-text-color="#0a60bd"
       :unique-opened="false"
-      background-color="#001529"
-      text-color="#B7BDC3"
-      active-text-color="#fff"
     >
       <template v-for="item in items">
         <!-- 包含子菜单 -->
-          <el-sub-menu :index="item.index" :key="item.index" v-if="item.subs">
+        <template v-if="item.children">
+          <el-submenu :index="item.index" :key="item.index">
             <template #title>
-              <i class="el-icon-location"></i>
-              <span>{{ item.title }}{{item.icon}}</span>
+              <i :class="item.icon"></i>
+              <span>{{ item.title }}</span>
             </template>
-            <template v-for="subItem in item.subs" :key="subItem.index">
-              <el-menu-item :index="subItem.index"
-                >{{ subItem.title }}
+            <template v-for="subItem in item.children" :key="subItem.index">
+              <el-menu-item :index="subItem.index">
+                <i :class="subItem.icon"></i>
+                <span>{{ subItem.title }}</span>
               </el-menu-item>
             </template>
-          </el-sub-menu>
+          </el-submenu>
+        </template>
         <!-- 只有一级菜单 -->
         <template v-else>
           <el-menu-item :index="item.index" :key="item.index">
             <i :class="item.icon"></i>
-            <span>{{ item.title }}{{item.icon}}</span>
+            <span>{{ item.title }}</span>
           </el-menu-item>
         </template>
       </template>
@@ -42,6 +46,12 @@
 import { defineComponent, ref, computed } from 'vue'
 
 export default defineComponent({
+  props: {
+    collapse: {
+      type: Boolean,
+      default: false
+    }
+  },
   setup() {
     const items = [
       {
@@ -50,50 +60,21 @@ export default defineComponent({
         title: '系统首页'
       },
       {
-        icon: 'el-icon-menu',
+        icon: 'el-icon-setting',
         index: '/table',
-        title: '基础表格'
+        title: '系统管理',
+        children: [
+          {
+            icon: 'el-icon-user-solid',
+            index: '/table',
+            title: '用户管理'
+          }
+        ]
       },
       {
-        icon: 'el-icon-menu',
+        icon: 'el-icon-goods',
         index: '/tabs',
-        title: 'tab选项卡'
-      },
-      {
-        icon: 'el-icon-menu',
-        index: '3',
-        title: '表单相关',
-        subs: [
-          {
-            index: '/form',
-            title: '基本表单'
-          },
-          {
-            index: '/upload',
-            title: '文件上传'
-          }
-        ]
-      },
-      {
-        icon: 'el-icon-menu',
-        index: '/icon',
-        title: '自定义图标'
-      },
-      {
-        icon: 'el-icon-menu',
-        index: '/i18n',
-        title: '国际化功能'
-      },
-      {
-        icon: 'el-icon-menu',
-        index: '7',
-        title: '错误处理',
-        subs: [
-          {
-            index: '/404',
-            title: '404页面'
-          }
-        ]
+        title: '商品信息'
       }
     ]
     return {
@@ -105,14 +86,14 @@ export default defineComponent({
 
 <style scoped lang='scss'>
 .mainSide {
-  background-color: #001529;
+  background-color: #0c2135;
+  height: 100%;
   min-width: 200px;
 }
 // logo 布局
 .logo {
   display: flex;
-  height: 28px;
-  padding: 12px 10px 8px 10px;
+  height: 50px;
   flex-direction: row;
   justify-content: flex-start;
   align-items: center;
@@ -132,10 +113,9 @@ export default defineComponent({
   text-align: center;
   line-height: 200px;
 }
-.mainSide > ul {
+::v-deep .el-menu {
   height: 100%;
+  border-right: none; // 去除右边框 避免出现错位情况
 }
-.mainSide i::before {
-  content: '';
-}
+
 </style>
