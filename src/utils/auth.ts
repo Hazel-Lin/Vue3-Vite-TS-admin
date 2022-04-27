8071beb4cce38201e7c91b80892ab715db9714af
linhuizi <529246402@qq.com>
2022-04-27 10:44:51 +0800

    build(configuration): add configuration

---
+++ src/utils/auth.ts
@@ -0,0 +1,13 @@
+const TokenKey = 'Hz-Admin'
+
+const getToken = function () {
+  return localStorage.getItem(TokenKey)
+}
+const setToken = function (token: any) {
+  return localStorage.setItem(TokenKey, token)
+}
+const removeToken = function () {
+  return localStorage.remremoveItemove(TokenKey)
+}
+
+export { getToken, setToken, removeToken }
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import type { UserConfig } from 'vite';
-import { resolve } from 'path';
+import type { UserConfig } from 'vite'
+import { resolve } from 'path'
 
-
+// 配置别名
 const alias: Record<string, string> = {
-	'@': resolve('/src/'),
-	'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
+	'@': resolve('/src/')
 };
 const viteConfig: UserConfig = {
-    plugins: [vue()],
+    plugins: [vue(
+    )],
     resolve: { alias },
 }
 
