c7ebcd1c7848563660a9a1ca756cb0c5599501a6
linhuizi <529246402@qq.com>
2022-07-31 20:46:39 +0800

    ✨ 组件使用中文

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <router-view></router-view>
+ <el-config-provider :locale="locale">
+    <router-view />
+  </el-config-provider>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
 export default defineComponent({
   name: "App",
   components: {
+  },
+  setup() {
+    return {
+      locale: zhCn,
+    }
   },
 });
 </script>
