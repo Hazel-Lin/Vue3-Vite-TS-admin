018b9e3a9b91c72e742b1b393b7b77c4d2a024b8
linhuizi <529246402@qq.com>
2022-08-02 18:31:11 +0800

    🚧  添加vitest单元测试

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,33 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import DefineOptions from 'unplugin-vue-define-options/vite'
 import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
+  test: {
+    include: ['test/**/*.test.ts'],
+    environment: 'jsdom',
+    deps: {
+      inline: ['@vue', '@vueuse', 'vue-demi'],
+    },
+  },
   plugins: [
     Vue({
       include: [/\.vue$/, /\.md$/],
     }),
     DefineOptions(),
     Unocss(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        'vue-i18n',
+        '@vueuse/head',
+        '@vueuse/core',
+        'vitest',
+      ],
+      dts: 'src/auto-imports.d.ts',
+    }),
   ],
   resolve: {
     // 配置别名
@@ -18,7 +37,9 @@ export default defineConfig({
       '@/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
+
   // 解决process is not defined的问题
   define: { 'process.env': {} },
+
 })
 
