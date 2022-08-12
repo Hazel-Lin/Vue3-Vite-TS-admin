4a1aef279c883752930fbbe834197aa2d7fb5b9f
linhuizi <529246402@qq.com>
2022-08-12 18:31:02 +0800

    🚧 修复打包构建问题

--- vite.config.ts
+++ vite.config.ts
@@ -7,52 +7,71 @@ import AutoImport from 'unplugin-auto-import/vite'
 import { viteMockServe } from 'vite-plugin-mock'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-export default defineConfig({
-  plugins: [
-    Vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
-    viteMockServe({
-      // default
-      mockPath: 'mock',
-      localEnabled: true,
-    }),
-    DefineOptions(),
-    Unocss(),
-    AutoImport({
-      imports: [
-        'vue',
-        'vue-router',
-        'vitest',
-      ],
-      dts: 'src/auto-imports.d.ts',
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    // 配置别名
-    alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
+import { loadEnv } from 'vite'
+import type { ConfigEnv, UserConfig } from 'vite'
+import dayjs from 'dayjs'
+import pkg from './package.json'
 
-  // 解决process is not defined的问题
-  define: { 'process.env': {} },
-  test: {
-    include: ['test/**/*.test.ts'],
-    environment: 'jsdom',
-    deps: {
-      inline: ['@vue', '@vueuse', 'vue-demi'],
+const CWD = process.cwd()
+const __APP_INFO__ = {
+  pkg,
+  lastBuildTime: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+}
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+  const { VITE_BASE_URL, VITE_DROP_CONSOLE } = loadEnv(mode, CWD)
+  return {
+    base: VITE_BASE_URL,
+    define: {
+      __APP_INFO__: JSON.stringify(__APP_INFO__),
     },
-  },
-  build: {
-    sourcemap: false,
-    // 消除打包大小超过500kb警告
-    chunkSizeWarningLimit: 4000,
-  },
+  }
+}
+// export default defineConfig({
+//   plugins: [
+//     Vue({
+//       include: [/\.vue$/, /\.md$/],
+//     }),
+//     viteMockServe({
+//       // default
+//       mockPath: 'mock',
+//       localEnabled: true,
+//     }),
+//     DefineOptions(),
+//     Unocss(),
+//     AutoImport({
+//       imports: [
+//         'vue',
+//         'vue-router',
+//         'vitest',
+//       ],
+//       dts: 'src/auto-imports.d.ts',
+//       resolvers: [ElementPlusResolver()],
+//     }),
+//     Components({
+//       resolvers: [ElementPlusResolver()],
+//     }),
+//   ],
+//   resolve: {
+//     // 配置别名
+//     alias: {
+//       '@/': `${path.resolve(__dirname, 'src')}/`,
+//     },
+//   },
 
-})
+//   // 解决process is not defined的问题
+//   // define: { 'process.env': {} },
+//   test: {
+//     include: ['test/**/*.test.ts'],
+//     environment: 'jsdom',
+//     deps: {
+//       inline: ['@vue', '@vueuse', 'vue-demi'],
+//     },
+//   },
+//   build: {
+//     sourcemap: false,
+//     // 消除打包大小超过500kb警告
+//     chunkSizeWarningLimit: 4000,
+//   },
+
+// })
 
